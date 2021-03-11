5a0c9589db89dc41ff9b54a05f3e9eafe8d551be
Manaswi Patil <manaswi.c.patil@gmail.com>
2021-03-11 18:19:08 +1300

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ async function getQuote()
 {
 	loading();
 
-	const proxyUrl = 'https://thingproxy.freeboard.io/fetch/';
+	const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
 	const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
 	try {
 		const response = await fetch(proxyUrl+ apiUrl);
